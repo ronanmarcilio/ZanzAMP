252a57f3d63782014fbeee2928f8ecd25bfc5d46
ronanmarcilio <ronanmarcilio@gmail.com>
2025-12-27 10:32:06 -0300

    Update PluginProcessor.h

--- ZanzAMP_Professional_Package/Source/PluginProcessor.h
+++ ZanzAMP_Professional_Package/Source/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
 
 class ZanzAudioProcessor  : public juce::AudioProcessor
 {
